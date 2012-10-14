3fe6b05debdd8a8d0dbebc26fd47f3dbda3f9980
Tomaz Muraus <tomaz@tomaz.me>
2012-10-13 20:29:06 -0700

    Bump the version.

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ class TestCommand(Command):
 
 setup(
     name='txServiceRegistry',
-    version='0.1.2',
+    version='0.1.3',
     description='A Twisted Python client for Rackspace Service Registry.',
     author='Rackspace Hosting, Inc.',
     author_email='sr@rackspace.com',
